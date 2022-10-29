a8d06c604af573ce55ac3637ffdb91db9ba8751d
Ashar Ali Khan <asharkhan7658@gmail.com>
2022-10-29 12:08:56 +0530

    Dark mode added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import LeftBar from "./components/leftbar/LeftBar";
 import RightBar from "./components/rightbar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 function App() {
 
   const currentUser = true;
+  const { darkMode } = useContext(DarkModeContext)
 
   const Layout = () => {
     return(
-      <div className="theme-dark">
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
         <div style={{ display: "flex"}}>
           <LeftBar />
